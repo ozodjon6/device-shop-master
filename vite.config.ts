1315c58c475d00f4768ea9962ecc685f6998d395
ozodbekjumaev <ozodjon6@gmail.com>
2024-03-23 00:14:17 +0500

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
-import vue from "@vitejs/plugin-vue";
-import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vite";
-import { createRequire } from "node:module";
-const require = createRequire(import.meta.url);
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'url'
+import { defineConfig } from 'vite'
+import { createRequire } from 'node:module'
+import { viteGhPages } from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/device-shop-master',
   plugins: [
     vue(),
+    viteGhPages({
+      domain: 'device-shop-master.github.io',
+      base: '/device-shop-master/'
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      "~": fileURLToPath(new URL("./src", import.meta.url)),
+      '~': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
