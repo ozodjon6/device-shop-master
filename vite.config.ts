4d2f7866b0fa1f487b04d60b1a38f9d25ec225e9
ozodbekjumaev <ozodjon6@gmail.com>
2024-03-23 00:15:07 +0500

    fix some change

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
-import { createRequire } from 'node:module'
-import { viteGhPages } from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/device-shop-master',
-  plugins: [
-    vue(),
-    viteGhPages({
-      domain: 'device-shop-master.github.io',
-      base: '/device-shop-master/'
-    })
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
